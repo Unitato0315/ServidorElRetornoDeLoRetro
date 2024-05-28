ab131201e66d8230da424ed84af86aec4dd0cab0
JoseVicente <104432189+Unitato0315@users.noreply.github.com>
2024-05-28 20:29:27 +0200

    Delete build directory

--- build/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PORT = process.env.PORT || 3000;
-exports.default = {
-    jwtSecret: 'BDPEK@',
-    server: {
-        port: PORT
-    }
-};
